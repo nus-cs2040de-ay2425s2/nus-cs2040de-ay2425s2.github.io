89ac690af8da36d2ad95fff0392050dbd47dfd52
Zikun Zhu <zhu.zikun@u.nus.edu>
2025-02-17 15:55:07 +0800

    fix: update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nus-cs2040de-ay2425s2.github.io/',
+  base: '/',
   build: {
     sourcemap: true
   }
